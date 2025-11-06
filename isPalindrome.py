f6cf32de15720ac840caf44312731228a94b286a
Oomat Latipov <oomatlatipov@Oomats-MacBook-Pro.local>
2025-11-06 00:16:17 -0500

    isPalindrome

---
+++ isPalindrome.py
@@ -0,0 +1,28 @@
+# isPalindrome completed by Oomat Latipov
+def isPalindrome():
+    
+    x = 10
+    string_x = str(x)
+    isPalindromeCheck = True
+    
+    start = 0
+    end = len(string_x) - 1
+    
+    while start < end:
+        if string_x[start] != string_x[end]:
+            isPalindromeCheck = False
+            break
+        start += 1
+        end -= 1
+    
+    print(isPalindromeCheck)
+
+isPalindrome()
+
+
+# optimized version of isPalindrome
+def isPalindrome():
+    x = 22233333222
+    print(str(x) == str(x)[::-1])
+
+isPalindrome()
