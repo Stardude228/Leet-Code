57e3b74c776efdf4c133e11c624dd9c56bd64621
Oomat Latipov <oomatlatipov@Oomats-MacBook-Pro.local>
2025-11-05 23:23:39 -0500

    twoSum

---
+++ twoSum.py
@@ -0,0 +1,21 @@
+# twoSum completed by Oomat Latipov
+nums = [3,2,3]
+def twoSum(nums):
+    target = 6
+    for a in range(len(nums) - 1):
+        for b in range(a + 1, len(nums)):
+            if nums[a] + nums[b] == target:
+                return [a, b]
+twoSum(nums)
+
+# optimized version of twoSum
+nums = [3,2,3]
+def twoSum(nums):
+    target = 6
+    mp = {}
+    for i,val in enumerate(nums):
+        diff = target - val
+        if diff in mp:
+            return [mp[diff],i]
+        mp[val] = i
+twoSum(nums)
